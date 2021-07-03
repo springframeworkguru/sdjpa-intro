97387411272f1a2ee0b59439ebea0a29a46a85b1
John Thompson <john@springframework.guru>
2021-07-03 11:28:42 -0400

    adding test splice

---
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,30 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Created by jt on 7/3/21.
+ */
+@DataJpaTest
+public class SpringBootJpaTestSlice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+
+        bookRepository.save(new Book("My Book", "1235555", "Self"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+}
