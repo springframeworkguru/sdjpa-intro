dbf1568d7171db0eff3c28a006f29323aa298dd1
John Thompson <john@springframework.guru>
2021-07-04 12:28:51 -0400

    adding mysql integration test

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,11 +3,13 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 6/12/21.
  */
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -19,13 +21,10 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-
-        System.out.println("Id: " + bookDDD.getId() );
-
         Book savedDDD = bookRepository.save(bookDDD);
-
-        System.out.println("Id: " + savedDDD.getId() );
 
         Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
         Book savedSIA = bookRepository.save(bookSIA);
---
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Created by jt on 7/4/21.
+ */
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySQLIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySQL() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+    }
+
+}
+
+
