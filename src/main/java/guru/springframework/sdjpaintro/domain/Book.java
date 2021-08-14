38390d9599fe1eec4b03619178837d02105aac3f
John Thompson <john@springframework.guru>
2021-08-14 14:53:38 -0400

    adding author id to book

--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -19,15 +19,17 @@ public class Book {
     private String title;
     private String isbn;
     private String publisher;
+    private Long authorId;
 
     public Book() {
 
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, String publisher, Long authorId) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+        this.authorId = authorId;
     }
 
     @Override
