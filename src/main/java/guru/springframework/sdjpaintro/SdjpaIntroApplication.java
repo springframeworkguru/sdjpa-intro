c6dd1acfd1311ee0335f9228ed2d60c65862f2f8
David Feingold <softintegrationdf@gmail.com>
2022-10-13 10:06:21 -0400

    Update

--- src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdjpaIntroApplication.java
@@ -1,5 +1,5 @@
 package guru.springframework.sdjpaintro;
-
+// this is test
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
