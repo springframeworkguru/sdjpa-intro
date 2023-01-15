9425541812a58803af1993e1ea7f4bbabab2fa75
Jörg-Christian Müller <jcm@shv-is.eu>
2023-01-15 16:45:21 +0100

    End Lession 13

---
+++ src/main/java/guru/springframework/sdjpaintro/repositories/BookRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 by SHV Consulting Integrated Services UG
+ * author: jcm on 2023/1/15
+ */
+
+package guru.springframework.sdjpaintro.repositories;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
