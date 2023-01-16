03b2cfe33b5cd3a3fd8f69e70b87a241cb5d988c
Jörg-Christian Müller <jcm@shv-is.eu>
2023-01-16 20:28:40 +0100

    End Lession 24: MySQL and SpringBoot

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,13 +1,19 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+    @Test
+    void testBookRepository() {
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
