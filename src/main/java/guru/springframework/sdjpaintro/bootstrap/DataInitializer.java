3d604b69d10e2ce7b82d569ba7f8530de057a88c
Jörg-Christian Müller <jcm@shv-is.eu>
2023-01-17 01:07:13 +0100

    End Lesson 34: H2 MySQL Compatibility Mode

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -8,8 +8,10 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local","default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -21,18 +23,18 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Book    bookDDD = new Book("Domain Driven Design", "123", "Random");
+        bookRepository.deleteAll();
 
-        System.out.println("id: " + bookDDD.getId());
-        Book    savedDDD = bookRepository.save(bookDDD);
-        System.out.println("id: " + savedDDD.getId());
+        Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+        Book savedDDD = bookRepository.save(bookDDD);
 
-        Book    bookSIA = new Book("Spring in Action", "234234", "O'Reilly");
-        Book    savedIA = bookRepository.save(bookSIA);
+        Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
+        Book savedSIA = bookRepository.save(bookSIA);
 
         bookRepository.findAll().forEach(book -> {
-            System.out.println("Book Id   : " + book.getId());
+            System.out.println("Book Id: " + book.getId());
             System.out.println("Book Title: " + book.getTitle());
         });
+
     }
 }
