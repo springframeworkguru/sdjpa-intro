37e3835ab04fdb7d9f49c11eee48e12fc52e5ebf
panos-geladaris <panagiotis.geladaris@transferwise.com>
2023-07-30 20:40:00 +0100

    adding simple domain pojo

---
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -0,0 +1,54 @@
+package guru.springframework.sdjpaintro.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+}
